86c2d243e0275a737da487e071205b6130218476
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-14 15:10:08 -0300

    config tailwind and add fonts/colors

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'antonio':['Antonio', 'sans-serif'],
+            'spartan':['League Spartan', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                'veryDarkBlue': '#070724',
+                'verySoftBlue':'#B1D5E2',
+                'lightGrayishBlue.':'#DEF4FC',
+            }, 
+        }
+    }
+}
