4e89d2fdaf4525d464eefbf13f66c11c0dbda868
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-14 15:53:33 -0300

    Star creation animation

---
+++ js/createStars.js
@@ -0,0 +1,34 @@
+function createStars () {
+    let star = document.createElement('div');
+    star.classList.add("bg-white/50", "rounded-full", "absolute");
+
+    let size = Math.random() * 8 + 2;
+    star.style.width = `${size}px`;
+    star.style.height = `${size}px`;
+
+    let posX = Math.random() * window.innerWidth;
+    let posY = Math.random() * window.innerHeight;
+
+    star.style.left = `${posX}px`;
+    star.style.top = `${posY}px`;
+
+    star.style.opacity = 0;
+    star.style.transition = 'opacity 1s ease-out';
+
+    document.getElementById('star-container').appendChild(star);
+
+    setTimeout(function () {
+      star.style.opacity = 1;
+    }, 10); 
+
+    setTimeout(function () {
+      star.style.opacity = 0;
+      setTimeout(function () {
+        document.body.removeChild(star);  
+      }, 1000); 
+    }, 1000); 
+}
+
+setInterval(function() {
+    createStars();
+}, 80);
